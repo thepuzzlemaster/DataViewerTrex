937ec009a9f081014a8bbbb28b09272f5290e896
iLan Epstein <iepstein@tableau.com>
2018-09-07 10:38:26 -0700

    Refactor and syntax cleanup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { ContextMenu, MenuItem } from 'react-contextmenu';
-import { Button, Glyphicon, Modal } from 'react-bootstrap';
-import { DataGrid, IndexBasedGridSelection, SelectionBehavior } from '@tableau/widgets-datagrid';
+import React from 'react'
+import { ContextMenu, MenuItem } from 'react-contextmenu'
+import { Button, Glyphicon, Modal } from 'react-bootstrap'
+import { DataGrid, IndexBasedGridSelection, SelectionBehavior } from '@tableau/widgets-datagrid'
 
-import LoadingIndicatorComponent from './LoadingIndicatorComponent';
-import ColumnListComponent from './ColumnListComponent';
-import DatasourceListComponent from './DatasourceListComponent';
-import { CopySelectionToClipboard } from './SelectionCopy';
-import { CreateCollectionClone } from './utils';
-import { CellFormatterFactory } from './CellFormatter';
-import ContextMenuTrigger from 'react-contextmenu/modules/ContextMenuTrigger';
+import LoadingIndicatorComponent from './LoadingIndicatorComponent'
+import ColumnListComponent from './ColumnListComponent'
+import DatasourceListComponent from './DatasourceListComponent'
+import { CopySelectionToClipboard } from './SelectionCopy'
+import { CreateCollectionClone } from './utils'
+import { CellFormatterFactory } from './CellFormatter'
+import ContextMenuTrigger from 'react-contextmenu/modules/ContextMenuTrigger'
 
-require('normalize.css/normalize.css');
-require('styles/App.scss');
-require('styles/React-contextmenu.scss');
-require('styles/Datagrid.scss');
+require('normalize.css/normalize.css')
+require('styles/App.scss')
+require('styles/React-contextmenu.scss')
+require('styles/Datagrid.scss')
 
 // Declare this so our linter knows that tableau is a global object
 /* global tableau */
 
 class AppComponent extends React.Component {
-  selection;
+  selection
 
   constructor (props) {
-    super(props);
+    super(props)
     this.state = {
       isFiltering: false,
       isLoading: true,
@@ -33,77 +33,77 @@ class AppComponent extends React.Component {
       headersCopy: [],
       dataKey: 1,
       filteredFields: []
-    };
+    }
 
-    this.unregisterEventFn = undefined;
-    this.dataSourceFetchPromises = [];
-    this.dashboardDataSources = {};
+    this.unregisterEventFn = undefined
+    this.dataSourceFetchPromises = []
+    this.dashboardDataSources = {}
   }
 
   getRowStore = () => {
     return {
       getItems: (start, end, rowHandler) => {
-        rowHandler(this.state.rows);
+        rowHandler(this.state.rows)
       }
     }
   }
 
   componentWillMount = () => {
     tableau.extensions.initializeAsync().then(() => {
-      const selectedDatasource = tableau.extensions.settings.get('datasource');
-      const dashboard = tableau.extensions.dashboardContent.dashboard;
-      const datasourceSelected = !!selectedDatasource;
-      let dataSourceNames = [];
+      const selectedDatasource = tableau.extensions.settings.get('datasource')
+      const dashboard = tableau.extensions.dashboardContent.dashboard
+      const datasourceSelected = !!selectedDatasource
+      let dataSourceNames = []
       this.setState({
         isLoading: datasourceSelected,
         selectedDatasource: selectedDatasource
-      });
+      })
 
       dashboard.worksheets.forEach(worksheet => {
-        this.dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
-      });
+        this.dataSourceFetchPromises.push(worksheet.getDataSourcesAsync())
+      })
 
       Promise.all(this.dataSourceFetchPromises).then(fetchResults => {
         fetchResults.forEach(dataSourcesForWorksheet => {
           dataSourcesForWorksheet.forEach(dataSource => {
             if (!this.dashboardDataSources[dataSource.name]) { // only add if not already there
-              this.dashboardDataSources[dataSource.name] = dataSource;
-              dataSourceNames.push(dataSource.name);
+              this.dashboardDataSources[dataSource.name] = dataSource
+              dataSourceNames.push(dataSource.name)
             }
-          });
-        });
+          })
+        })
 
         this.setState({
           isLoading: false,
           datasourceNames: dataSourceNames
-        });
+        })
 
         if (datasourceSelected) {
-          this.loadSelectedData();
+          this.loadSelectedData()
         }
-      });
-    });
+      })
+    })
   }
 
   getSelectedSheet (selectedSheet) {
-    const sheetName = selectedSheet || this.state.selectedDatasource;
-    return tableau.extensions.dashboardContent.dashboard.worksheets.find(worksheet => worksheet.name === sheetName);
+    const sheetName = selectedSheet || this.state.selectedDatasource
+    return tableau.extensions.dashboardContent.dashboard.worksheets.find(worksheet => worksheet.name === sheetName)
   }
 
   onSelectDatasource = (datasourceName) => {
-    tableau.extensions.settings.set('datasource', datasourceName);
-    this.setState({ isLoading: true });
+    tableau.extensions.settings.set('datasource', datasourceName)
+    this.setState({ isLoading: true })
     tableau.extensions.settings.saveAsync().then(() => {
-      this.setState({ selectedDatasource: datasourceName, filteredFields: [] }, this.loadSelectedData);
-    });
+      this.setState({ selectedDatasource: datasourceName, filteredFields: [] }, this.loadSelectedData)
+    })
   }
 
   loadSelectedData () {
     if (this.unregisterEventFn) {
-      this.unregisterEventFn();
+      this.unregisterEventFn()
     }
 
-    const datasource = this.dashboardDataSources[this.state.selectedDatasource];
+    const datasource = this.dashboardDataSources[this.state.selectedDatasource]
     datasource.getUnderlyingDataAsync().then(returnedData => {
 
       const headers = returnedData.columns.map((column, index) => {
@@ -115,18 +115,18 @@ class AppComponent extends React.Component {
           cellFormatter: CellFormatterFactory,
           isVisible: true
         }
-      });
+      })
 
       const rows = returnedData.data.map((row, rowIndex) => {
         let mappedRow = {
           data: {},
           key: rowIndex
-        };
+        }
 
         row.forEach((cell, cellIndex) => {
-          let fieldName = (headers[cellIndex] && headers[cellIndex].headerText) || '';
-          let value = cell.formattedValue;
-          mappedRow.data[fieldName] = value;
+          let fieldName = (headers[cellIndex] && headers[cellIndex].headerText) || ''
+          let value = cell.formattedValue
+          mappedRow.data[fieldName] = value
         })
         return mappedRow
       })
@@ -146,9 +146,9 @@ class AppComponent extends React.Component {
     })
 
     // this.unregisterEventFn = dashboard.addEventListener(tableau.TableauEventType.MarkSelectionChanged, () => {
-    //   this.setState({ isLoading: true });
-    //   this.loadSelectedData();
-    // });
+    //   this.setState({ isLoading: true })
+    //   this.loadSelectedData()
+    // })
   }
 
   handleSelectionEvent = (event) => {
@@ -191,55 +191,51 @@ class AppComponent extends React.Component {
   }
 
   onResetFilters () {
-    const worksheet = this.getSelectedSheet();
-    this.setState({ isLoading: true });
-    const promises = this.state.filteredFields.map(fieldName => worksheet.clearFilterAsync(fieldName));
+    const worksheet = this.getSelectedSheet()
+    this.setState({ isLoading: true })
+    const promises = this.state.filteredFields.map(fieldName => worksheet.clearFilterAsync(fieldName))
     Promise.all(promises).then(() => {
-      this.setState({ filteredFields: [], isLoading: false });
-    });
+      this.setState({ filteredFields: [], isLoading: false })
+    })
   }
 
   handleCopy = () => {
-    const cellRanges = this.selection.selection.cellRanges;
-    const {headers, rows} = this.state;
+    const cellRanges = this.selection.selection.cellRanges
+    const {headers, rows} = this.state
 
-    CopySelectionToClipboard(cellRanges, headers, rows);
+    CopySelectionToClipboard(cellRanges, headers, rows)
   }
 
-  render = () => {
-    if (this.state.isLoading) {
-      return (<LoadingIndicatorComponent msg='Loading' />);
-    }
+  renderDataSourceSelector = () => {
+    return (
+      <Modal show={true}>
+        <Modal.Header>
+          <Modal.Title>Choose a Data Source</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <DatasourceListComponent datasourceNames={this.state.datasourceNames} onSelectDatasource={this.onSelectDatasource} />
+        </Modal.Body>
+      </Modal>)
+  }
 
-    if (!this.state.selectedDatasource) {
-      return (
-        <Modal show={true}>
-          <Modal.Header>
-            <Modal.Title>Choose a Data Source</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <DatasourceListComponent datasourceNames={this.state.datasourceNames} onSelectDatasource={this.onSelectDatasource} />
-          </Modal.Body>
-        </Modal>);
-    }
+  renderColumnFilter = () => {
+    return (
+      <Modal show={true} onHide={this.cancelFiltering}>
+        <Modal.Header closeButton={true}>
+          <Modal.Title>Select Columns to Display</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ColumnListComponent headersCopy={this.state.headersCopy} onColumnToggled={this.onColumnToggled}></ColumnListComponent>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.cancelFiltering}>Cancel</Button>
+          <Button onClick={this.saveFiltering} bsStyle="primary">Save Changes</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 
-    if (this.state.isFiltering) {
-      return (
-        <Modal show={true} onHide={this.cancelFiltering}>
-          <Modal.Header closeButton={true}>
-            <Modal.Title>Select Columns to Display</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ColumnListComponent headersCopy={this.state.headersCopy} onColumnToggled={this.onColumnToggled}></ColumnListComponent>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.cancelFiltering}>Cancel</Button>
-            <Button onClick={this.saveFiltering} bsStyle="primary">Save Changes</Button>
-          </Modal.Footer>
-        </Modal>
-      );
-    }
-
+  renderDataGrid = () => {
     const gridProps = {
       cols: this.state.headers.filter(header => header.isVisible),
       rowStore: this.getRowStore(),
@@ -249,11 +245,11 @@ class AppComponent extends React.Component {
       showRowBanding: true,
       showRowHeaders: true,
       showVerticalGridLines: true
-    };
+    }
 
     const mainContent = this.state.rows.length > 0
       ? (<DataGrid.element {...gridProps} />)
-      : (<h4>No data found</h4>);
+      : (<h4>No data found</h4>)
 
     return (
       <div>
@@ -273,8 +269,24 @@ class AppComponent extends React.Component {
           <MenuItem onClick={this.handleCopy}>Copy</MenuItem>
         </ContextMenu>
       </div>
-    );
+    )
+  }
+
+  render = () => {
+    if (this.state.isLoading) {
+      return (<LoadingIndicatorComponent msg='Loading' />)
+    }
+
+    if (!this.state.selectedDatasource) {
+      return this.renderDataSourceSelector()
+    }
+
+    if (this.state.isFiltering) {
+      return this.renderColumnFilter()
+    }
+
+    return this.renderDataGrid()
   }
 }
 
-export default AppComponent;
+export default AppComponent
